d6713547939a333b3d83870931997cccb49ffe03
EchoKes <iamkester1@gmail.com>
2020-05-09 11:52:43 +0800

    Completed practical 3

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -2,7 +2,14 @@ package sg.edu.np.mad.week3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.telecom.InCallService;
+import android.widget.VideoView;
+
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VideoView video = findViewById(R.id.videoView);
+        MediaPlayer media  = MediaPlayer.create(this, R.raw.baby_laughing);
+        media.start();
+
+        if(media.isPlaying() != true){      //play audio only once
+            media.release();
+            media = null;
+        }
+
+        video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.rabbid));
+        video.start();
     }
 }
